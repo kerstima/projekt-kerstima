3874dec16ab944182600d01e78d1fd72b313991c
kerstima <kersti.maurer@gmail.com>
2015-12-01 22:14:57 +0200

    endiselt ei toimi

--- src/Toidupaevik/tervitus.java
+++ src/Toidupaevik/tervitus.java
@@ -4,19 +4,14 @@ package Toidupaevik;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Polina on 28.11.2015.
- */
 public class tervitus extends Application {
 
     Stage window;
@@ -24,6 +19,7 @@ public class tervitus extends Application {
     Button button;
     Label resultLabel;
     Label resultLabel1;
+    ListView<String>listView;
 
 
     public tervitus() {}
@@ -35,9 +31,15 @@ public class tervitus extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
 
-       punnid box1 = new punnid("Rasvata piim");
+        window = primaryStage;
+        resultLabel = new Label();
+        resultLabel1=new Label();
+        this.button = new Button("Arvuta");
+
+        //listView = new ListView<>();
+
+        punnid box1 = new punnid("Rasvata piim");
         intValueCheckboxList.add(box1);
         punnid box2 = new punnid("1% piim");
         intValueCheckboxList.add(box2);
@@ -47,19 +49,28 @@ public class tervitus extends Application {
         intValueCheckboxList.add(box4);
         punnid box5 = new punnid("sai, keskmine viil, 30g");
         intValueCheckboxList.add(box5);
+        punnid box6 = new punnid("Rasvata piim");
+        intValueCheckboxList.add(box6);
+        punnid box7 = new punnid("1% piim");
+        intValueCheckboxList.add(box7);
+        punnid box8 = new punnid("2,5% piim");
+        intValueCheckboxList.add(box8);
+        punnid box9 = new punnid("leib, keskmine viil, 30g");
+        intValueCheckboxList.add(box9);
+        punnid box10 = new punnid("sai, keskmine viil, 30g");
+        intValueCheckboxList.add(box10);
 
-        resultLabel = new Label();
-        resultLabel1=new Label();
-
-
-        this.button = new Button("Arvuta");
-        this.button.setOnAction(( event) -> {
-            this.calculate();
-        });
+       // listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(tervitus,box1, box2, box3,box4,box5, resultLabel,resultLabel1, this.button);
+        layout.getChildren().addAll(tervitus,box1, box2, box3,box4,box5,box6, box7, box8,box9,box10, resultLabel,resultLabel1, this.button);
+
+
+        this.button.setOnAction((event) -> {
+            this.calculate();
+        });
+
         this.scene = new Scene(layout, 500, 500);
         this.window.setScene(this.scene);
         this.window.show();
