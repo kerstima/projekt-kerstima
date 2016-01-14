eb8f296a72eaf880f5bb7d2f9a853896f4219ba6
kerstima <kersti.maurer@gmail.com>
2016-01-14 21:44:53 +0200

    Update Punnid.java
    Projektiparandus

--- src/Toidupaevik/Punnid.java
+++ src/Toidupaevik/Punnid.java
@@ -1,25 +1,23 @@
 package Toidupaevik;
 
 import javafx.scene.control.CheckBox;
-//klass võimaldab lisada toiduainele numbrilist väärtust (getter-setter), et saaks kasutada arvutamise meetodit
+
+
 public class Punnid extends CheckBox {
+
     private int value;
+
 
     public int getValue() {
         return value;
     }
-    public void setValue(int value) {
-        this.value = value;
-    }
 
-    public Punnid(String text, int value) {
+    public Punnid (String text, int value) {
         super(text);
         this.value = value;
     }
-
 
     public Punnid (String text) {
         super(text);
     }
-
 }
